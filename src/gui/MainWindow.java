28ecbcf794835e8de220689f9fa164616a4343ae
OxyTom <oxytom8@gmail.com>
2020-04-19 07:15:03 +0200

    added firsts class for the gui

---
+++ src/gui/MainWindow.java
@@ -0,0 +1,16 @@
+package gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MainWindow extends JFrame implements ActionListener {
+    public MainWindow() {
+        super("titre");
+        setLocation(300, 200);
+        setSize(1280,720);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+    }
+}
---
+++ src/tests/TestMainWindow.java
@@ -0,0 +1,10 @@
+package tests;
+
+import gui.MainWindow;
+
+public class TestMainWindow {
+    public static void main(String[] args) {
+        var win = new MainWindow();
+        win.setVisible(true);
+    }
+}
