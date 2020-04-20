31bbab14bde0a944b153fe67c8efbd8995541f9a
OxyTom <oxytom8@gmail.com>
2020-04-20 11:02:02 +0200

    list of user is now showing

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -5,22 +5,26 @@ import java.util.ArrayList;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.event.*;
 
 import app.*;
 
-public class MainWindow extends JFrame implements ActionListener {
-    protected JList<Client> clients;
+public class MainWindow extends JFrame implements ActionListener, ListSelectionListener {
+    protected DefaultListModel<Client> clients;
 
     private boolean dialogShowing = false;
 
     private JPanel pnl_clients;
     private JButton btn_newCommande;
     private JButton btn_deluser;
+    private JList<Client> l_clients;
 
     public MainWindow() {
         super("titre");
         setLocation(300, 200);
         setSize(1280, 720);
+
+        clients = new DefaultListModel<Client>();
 
         initComponents();
     }
@@ -37,10 +41,15 @@ public class MainWindow extends JFrame implements ActionListener {
         toolbar.addSeparator();
 
         pnl_clients = new JPanel(new FlowLayout());
-        clients = new JList<Client>();
-        add(clients, BorderLayout.EAST);
+        l_clients = new JList<Client>(clients);
+        l_clients.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        l_clients.setSelectedIndex(0);
+        l_clients.addListSelectionListener(this);
+        l_clients.setVisibleRowCount(5);
+        var l_clientsScrollPane = new JScrollPane(l_clients);
 
         add(toolbar, BorderLayout.NORTH);
+        add(l_clientsScrollPane, BorderLayout.EAST);
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -54,5 +63,9 @@ public class MainWindow extends JFrame implements ActionListener {
 
     public void commandeDialogReturn() {
         dialogShowing = false;
+    }
+
+    public void valueChanged(ListSelectionEvent e) {
+
     }
 }
