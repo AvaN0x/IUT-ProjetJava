0fe7719df1a917c641364de3f93112dc039c2340
OxyTom <oxytom8@gmail.com>
2020-04-28 16:08:16 +0200

    documentation for the logstream

--- src/gui/LogStream.java
+++ src/gui/LogStream.java
@@ -7,6 +7,10 @@ import java.util.Date;
 public class LogStream {
     private PrintWriter writer;
 
+    /**
+     * Create an new logger, to log some usefull infos, like exceptions.
+     * @param path the path of the file
+     */
     public LogStream(String path){
         try{
             if((new File(path)).exists())
@@ -21,19 +25,35 @@ public class LogStream {
         }
     }
 
+    /**
+     * To close the stream
+     */
     public void close(){
         writer.close();
     }
 
+    /**
+     * If you need to log an error
+     * @param value the thing that need to be logged
+     */
     public void Error(Object value) {
         Log(value, "ERROR");
     }
 
+    /**
+    *  If you need to log an information
+    *  @param value the thing that need to be logged
+    */
     public void Log(Object value){
         writer.println("[INFO] " + new SimpleDateFormat("HH:mm:ss").format(new Date()) + " - " + value.toString());
         writer.flush();
     }
 
+    /**
+     * If you need to log something else
+     * @param value the thing that need to be logged
+     * @param status the level of priority of that thing
+     */
     public void Log(Object value, String status){
         writer.println("[" + status + "] " + new SimpleDateFormat("HH:mm:ss").format(new Date()) + " - " + value.toString());
         writer.flush();
