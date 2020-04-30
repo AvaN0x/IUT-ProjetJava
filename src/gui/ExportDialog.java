26a7e7bd271f64d9d530f28d8254350180ac6994
OxyTom <oxytom8@gmail.com>
2020-04-30 16:03:44 +0200

    ready to export in differents type of files

--- src/gui/ExportDialog.java
+++ src/gui/ExportDialog.java
@@ -11,15 +11,15 @@ import javax.swing.*;
 import app.Commande;
 
 @SuppressWarnings("serial")
-public class ExportDialog extends JDialog implements ActionListener, ItemListener {
+public class ExportDialog extends JDialog implements ActionListener {
     private Commande commande;
-    
+
     private JComboBox<String> cbx_type;
 
     private JButton btn_valider;
     private JButton btn_cancel;
 
-    public ExportDialog(Window owner, Commande commande){
+    public ExportDialog(Window owner, Commande commande) {
         super(owner, "Gestion vidéothèque - Exporter la commande");
         setLocation(300, 200);
         setSize(200, 150);
@@ -32,16 +32,13 @@ public class ExportDialog extends JDialog implements ActionListener, ItemListene
     private void initComponents() {
         setLayout(new BorderLayout());
 
-        // TODO exporter dans d'autres formats
-
         var pnl_content = new JPanel();
         pnl_content.setLayout(new BoxLayout(pnl_content, BoxLayout.PAGE_AXIS));
-        
+
         cbx_type = new JComboBox<String>();
         cbx_type.addItem("txt file");
-        cbx_type.addItem("cxv file");
+        cbx_type.addItem("csv file");
         cbx_type.addItem("pdf file");
-        cbx_type.addItemListener(this);
 
         pnl_content.add(cbx_type);
 
@@ -55,56 +52,88 @@ public class ExportDialog extends JDialog implements ActionListener, ItemListene
         pnl_validate.add(btn_valider);
         pnl_validate.add(btn_cancel);
 
+        add(pnl_content, BorderLayout.CENTER);
         add(pnl_validate, BorderLayout.SOUTH);
     }
 
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == btn_valider) {
-            // TODO exporter la commande mieux, autres formats
-            try {
-                var file = new File("bin/commande_" + commande.getId() + ".txt");
-                if (!file.getParentFile().exists())
-                    file.getParentFile().mkdir();
-                if (file.exists())
-                    file.createNewFile();
-                var writer = new PrintWriter(file.getPath());
-
-                writer.println("Commande " + commande.getId());
-                writer.println("Client   " + commande.getClient().getId());
-                writer.println("\t\t " + commande.getClient().getNom() + " " + commande.getClient().getPrenom() + "\n");
-                
-                writer.println("Nombre d'emprunts : " + commande.getEmprunts().size());
-                for (var emprunt : commande.getEmprunts()) {
-                    writer.println(emprunt.getProduit().getTitle());
-                    writer.println("\t " + emprunt.getProduit().getOption1());
-                    writer.println("\t Type : " + emprunt.getProduit().getClass().getSimpleName());
-                    writer.println("\t Prix journalier : " + emprunt.getProduit().getDailyPrice() + "€");
-                    writer.println("\t Coût : " + emprunt.getCost() + "€");
-                    writer.println("\t " + Utils.dateToString(commande.getDateCreation()) + " -> " + Utils.dateToString(emprunt.getDateFin()) + " (" + Duration.between(commande.getDateCreation().toInstant(), emprunt.getDateFin().toInstant()).toDays() + " jours)"); // TODO afficher nombre de jours
-                }
-                writer.println("\nTotal HR: " + commande.getTotalCostNoReduc() + "€");
-                writer.println("Réduction : " + commande.getReduction() * 100 + "%");
-                writer.println("Total  : " + commande.getTotalCost() + "€");
-
-                writer.flush();
-                Utils.logStream.Log("Order " + commande.getId() + " exported");
-            } catch (FileNotFoundException ex) {
-                Utils.logStream.Error(ex);
-            } catch (IOException ex) {
-                Utils.logStream.Error(ex);
-            }
-
+            if (cbx_type.getSelectedIndex() == 0) // csv file
+                exportToCSV();
+            else if (cbx_type.getSelectedIndex() == 0) // pdf file
+                exportToPDF();
+            else // txt file
+                exportToTXT();
             quit();
         } else if (e.getSource() == btn_cancel) {
             quit();
         }
     }
 
-    public void itemStateChanged(ItemEvent e){
+    
+    private void exportToCSV() {
+        try {
+            var file = new File("bin/commande_" + commande.getId() + ".csv");
+            Utils.createFileIfNotExists(file);
+            var writer = new PrintWriter(file.getPath());
+            // TODO export in csv
+        } catch (FileNotFoundException ex) {
+            Utils.logStream.Error(ex);
+        } catch (IOException ex) {
+            Utils.logStream.Error(ex);
+        }
+    }
+    
+    private void exportToPDF() {
+        try {
+            var file = new File("bin/commande_" + commande.getId() + ".pdf");
+            Utils.createFileIfNotExists(file);
+            var writer = new PrintWriter(file.getPath());
+            // TODO export in pdf
+        } catch (FileNotFoundException ex) {
+            Utils.logStream.Error(ex);
+        } catch (IOException ex) {
+            Utils.logStream.Error(ex);
+        }
+    }
+    
+    private void exportToTXT() {
+        try {
+            var file = new File("bin/commande_" + commande.getId() + ".txt");
+            Utils.createFileIfNotExists(file);
+            var writer = new PrintWriter(file.getPath());
 
+            writer.println("Commande " + commande.getId());
+            writer.println("Client   " + commande.getClient().getId());
+            writer.println("\t\t " + commande.getClient().getNom() + " " + commande.getClient().getPrenom() + "\n");
+
+            writer.println("Nombre d'emprunts : " + commande.getEmprunts().size());
+            for (var emprunt : commande.getEmprunts()) {
+                writer.println(emprunt.getProduit().getTitle());
+                writer.println("\t " + emprunt.getProduit().getOption1());
+                writer.println("\t Type : " + emprunt.getProduit().getClass().getSimpleName());
+                writer.println("\t Prix journalier : " + emprunt.getProduit().getDailyPrice() + "€");
+                writer.println("\t Coût : " + emprunt.getCost() + "€");
+                writer.println("\t " + Utils.dateToString(commande.getDateCreation()) + " -> "
+                        + Utils.dateToString(emprunt.getDateFin()) + " ("
+                        + Duration.between(commande.getDateCreation().toInstant(), emprunt.getDateFin().toInstant())
+                                .toDays()
+                        + " jours)"); // TODO afficher nombre de jours
+            }
+            writer.println("\nTotal HR: " + commande.getTotalCostNoReduc() + "€");
+            writer.println("Réduction : " + commande.getReduction() * 100 + "%");
+            writer.println("Total  : " + commande.getTotalCost() + "€");
+
+            writer.flush();
+            Utils.logStream.Log("Order " + commande.getId() + " exported");
+        } catch (FileNotFoundException ex) {
+            Utils.logStream.Error(ex);
+        } catch (IOException ex) {
+            Utils.logStream.Error(ex);
+        }
     }
 
-    private void quit(){
+    private void quit() {
         setVisible(false);
         var owner = (IMyDialogOwner) getOwner();
         owner.dialogReturn();
