9723351168ab2d6a254facd9f6e7caa2d4f12ce6
OxyTom <oxytom8@gmail.com>
2020-05-17 12:29:41 +0200

    started settings dialog

---
+++ src/gui/SettingsDialog.java
@@ -0,0 +1,18 @@
+package gui;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+@SuppressWarnings("serial")
+public class SettingsDialog extends MyJDialog {
+    public SettingsDialog(Window owner) {
+        super(owner, "Paramètres", new Dimension(240, 210));
+    }
+
+    public void initComponents() {
+
+    }
+
+
+}
