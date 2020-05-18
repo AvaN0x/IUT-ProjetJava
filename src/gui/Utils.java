4566ccfd15117af73647322e888e9faf9f839306
OxyTom <oxytom8@gmail.com>
2020-05-18 18:18:58 +0200

    arrays start at 1 --'

--- src/gui/Utils.java
+++ src/gui/Utils.java
@@ -57,16 +57,16 @@ public class Utils {
      * @throws SQLException
      */
     static ResultSet SQLrequest(String request) throws SQLException{
-        try {
-            Class.forName("com.mysql.jdbc.Driver"); //TODO importer ce driver de con
+        //try {
+        //    Class.forName("com.mysql.jdbc.Driver"); //TODO importer ce driver de con
             Connection connect; Statement stmt;
             connect = DriverManager.getConnection(settings.getdbUrl(), settings.dbUser, settings.dbPass);
             stmt = connect.createStatement();
             return stmt.executeQuery(request);
-        } catch (ClassNotFoundException e) {
-            logStream.Error(e);
-        }
-        return null;
+        //} catch (ClassNotFoundException e) {
+        //    logStream.Error(e);
+        //}
+        //return null;
     } 
     
     /**
@@ -108,13 +108,6 @@ public class Utils {
 
             settings = (Settings) input.readObject();
 
-
-            try {
-                SQLrequest("SELECT * FROM `produits` NATURAL JOIN `types` WHERE categ = \"BD\"");
-            } catch (Exception e) {
-                logStream.Error(e);
-            }
-
             if(settings.isLocal) {
                 commandes.setList((List<Commande>) input.readObject());
 
@@ -125,54 +118,54 @@ public class Utils {
                 try{
                     var products = SQLrequest("SELECT * FROM `produits` NATURAL JOIN `types` WHERE categ = \"BD\"");
                     while (products.next())
-                        produits.add(new BD(products.getString(0), products.getString(1), products.getDouble(2), products.getInt(3), products.getString(4)));
+                        produits.add(new BD(products.getString(1), products.getString(2), products.getDouble(3), products.getInt(4), products.getString(5)));
 
                     products = SQLrequest("SELECT * FROM `produits` NATURAL JOIN `types` WHERE categ = \"Roman\"");
                     while (products.next())
-                        produits.add(new BD(products.getString(0), products.getString(1), products.getDouble(2), products.getInt(3), products.getString(4)));
+                        produits.add(new BD(products.getString(1), products.getString(2), products.getDouble(3), products.getInt(4), products.getString(5)));
 
                     products = SQLrequest("SELECT * FROM `produits` NATURAL JOIN `types` WHERE categ = \"Manuel Scolaire\"");
                     while (products.next())
-                        produits.add(new BD(products.getString(0), products.getString(1), products.getDouble(2), products.getInt(3), products.getString(4)));
+                        produits.add(new BD(products.getString(1), products.getString(2), products.getDouble(3), products.getInt(4), products.getString(5)));
 
                     products = SQLrequest("SELECT * FROM `produits` NATURAL JOIN `types` WHERE categ = \"Dictionnaire\"");
                     while (products.next())
-                        produits.add(new BD(products.getString(0), products.getString(1), products.getDouble(2), products.getInt(3), products.getString(4)));
+                        produits.add(new BD(products.getString(1), products.getString(2), products.getDouble(3), products.getInt(4), products.getString(5)));
 
                     products = SQLrequest("SELECT * FROM `produits` NATURAL JOIN `types` WHERE categ = \"CD\"");
                     while (products.next())
-                        produits.add(new BD(products.getString(0), products.getString(1), products.getDouble(2), products.getInt(3), products.getString(4)));
+                        produits.add(new BD(products.getString(1), products.getString(2), products.getDouble(3), products.getInt(4), products.getString(5)));
 
                     products = SQLrequest("SELECT * FROM `produits` NATURAL JOIN `types` WHERE categ = \"DVD\"");
                     while (products.next())
-                        produits.add(new BD(products.getString(0), products.getString(1), products.getDouble(2), products.getInt(3), products.getString(4)));
+                        produits.add(new BD(products.getString(1), products.getString(2), products.getDouble(3), products.getInt(4), products.getString(5)));
                 
                     var users = SQLrequest("SELECT * FROM `clients`");
                     while (users.next()){
                         if (users.getInt(3) == 1)
-                            clients.addElement(new ClientFidele(users.getString(0), users.getString(1), users.getString(2)));
+                            clients.addElement(new ClientFidele(users.getString(1), users.getString(2), users.getString(3)));
                             else
-                            clients.addElement(new ClientOccas(users.getString(0), users.getString(1), users.getString(2)));
+                            clients.addElement(new ClientOccas(users.getString(1), users.getString(2), users.getString(3)));
 
                     }
 
                     var orders = SQLrequest("SELECT * FROM `commandes`");
                     while (orders.next()){
                         Commande order = null;
-                        for (int i=0;i<clients.getSize();i++)
-                            if(clients.get(i).getId() == orders.getString(1)) {
+                        for (int i=1;i<clients.getSize();i++)
+                            if(clients.get(i).getId() == orders.getString(2)) {
                                 var cal = Calendar.getInstance();
-                                cal.setTime(orders.getDate(2));
-                                order = new Commande(orders.getString(0), clients.get(i), cal);
+                                cal.setTime(orders.getDate(3));
+                                order = new Commande(orders.getString(1), clients.get(i), cal);
                                 break;
                             }
-                        var loans = SQLrequest("SELECT * FROM `emprunts` WHERE id-empr = \""+ orders.getString(3) + "\"");
+                        var loans = SQLrequest("SELECT * FROM `emprunts` WHERE id-empr = \""+ orders.getString(4) + "\"");
                         while(loans.next()){
                             for (var produit : produits.getList())
-                                if (produit.getId() == loans.getString(3)) {
+                                if (produit.getId() == loans.getString(4)) {
                                     var cal = Calendar.getInstance();
-                                    cal.setTime(loans.getDate(2));
-                                    order.addEmprunt(loans.getString(0), cal, produit);
+                                    cal.setTime(loans.getDate(3));
+                                    order.addEmprunt(loans.getString(1), cal, produit);
                                 }
                         }
                         commandes.add(order);
